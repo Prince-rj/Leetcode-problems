6e730290db58ba82bf25fc9f9893ca806782188c
Prince-rj <88528048+Prince-rj@users.noreply.github.com>
2025-02-18 00:18:57 -0500

    Time: 1 ms (75.25%) | Memory: 41.7 MB (62.62%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,28 +1,15 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        List<String> li=new ArrayList<>();
-        help(new boolean[10],pattern,-1,-1,new StringBuilder(),li);
-        return li.get(0);
-    }
-    public void help(boolean used[],String pattern,int ind,int prev,StringBuilder num,List<String> fin){
-        if(ind==pattern.length()){
-            // System.out.println(num);
-            fin.add(num.toString());
-            return;
-        }
-        for(int i=1;i<=9;i++){
-            if(prev!=-1&&pattern.charAt(ind)=='I'){
-                if(i<=prev)continue;
+        Stack<Integer> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<=pattern.length();i++){
+            st.push(i+1);
+            if(i==pattern.length()||pattern.charAt(i)=='I'){
+                while(!st.isEmpty()){
+                    sb.append(st.pop());
+                }
             }
-            else{
-                if(prev!=-1&&i>=prev)continue;
-            }
-            if(used[i]==true)continue;
-            num.append(i);
-            used[i]=true;
-            help(used,pattern,ind+1,i,num,fin);
-            used[i]=false;
-            num.setLength(num.length()-1);
         }
+        return sb.toString();
     }
 }
