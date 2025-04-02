14d939422ad78cc490fa9ce272cf877bbf79f484
Prince-rj <88528048+Prince-rj@users.noreply.github.com>
2025-04-02 00:30:11 -0400

    Time: 2355 ms (5.06%) | Memory: 47.5 MB (5.08%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,16 +15,28 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
+        HashMap<TreeNode, Integer> hm=new HashMap<>();
+        return diam(root,hm);
+    }
+    public int diam(TreeNode root,HashMap<TreeNode,Integer> hm){
         if(root==null)return 0;
-        int curr=help(root.left)+help(root.right);
-        int l=diameterOfBinaryTree(root.left);
-        int r=diameterOfBinaryTree(root.right);
+        int x=0;
+        int y=0;
+        if(!hm.containsKey(x))x=help(root.left,hm);
+        else x=hm.get(root.left);
+        if(!hm.containsKey(y))y=help(root.right,hm);
+        else x=hm.get(root.right);
+        int curr=x+y;
+        int l=diam(root.left,hm);
+        int r=diam(root.right,hm);
         return Math.max(curr,Math.max(l,r));
     }
-    public int help(TreeNode root){
+    public int help(TreeNode root,HashMap<TreeNode,Integer> hm){
         if(root==null)return 0;
-        int l=help(root.left);
-        int r=help(root.right);
-        return 1+Math.max(l,r);
+        int l=help(root.left,hm);
+        int r=help(root.right,hm);
+        int v=1+Math.max(l,r);
+        hm.put(root,v);
+        return v;
     }
 }
