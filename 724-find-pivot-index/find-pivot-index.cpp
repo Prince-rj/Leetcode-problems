8b497f27dceed78afe251577ef796b67759bcac0
Prince-rj <88528048+Prince-rj@users.noreply.github.com>
2025-10-12 18:31:34 +0530

    Time: 108 ms (11.60%) | Memory: 36.4 MB (21.76%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int> temp(nums.size());
+        int prev=0;
+        for(int i=0;i<nums.size();i++){
+            temp[i]=prev+nums[i];
+            prev=temp[i];
+        }
+        for(auto i:temp)cout<<i;
+        for(int i=0;i<nums.size();i++){
+            int a=temp[i]-nums[i];
+            int b=temp[nums.size()-1]-temp[i];
+            if(a==b)return i;
+        }
+        return -1;
+    }
+};
